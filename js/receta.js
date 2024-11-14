8b9a79c4bef87e4fb1ccd03d5b2bae7f851db6ce
Vizanardi <mzanardiocampo@udesa.edu.ar>
2024-11-14 17:44:45 -0300

    Recetas.index y js primera edicion

---
+++ js/receta.js
@@ -0,0 +1,25 @@
+let queryString = location.search; //esto nos devuelve la inormacion que sacamos del api o de la pagina en un formato no legible 
+let queryStringObjeto = new URLSearchParams(queryString); //este new Url.. lo que hace es que se convierta en un objeto y pueda ser manejado despues con get por ejemplo
+
+let identificador = queryStringObjeto.get('id'); //CHEQUEAR SI ES ID de esta manera podemos usar el get y obtener ese dato (id) del array de informacion que sacamos de la web
+
+console.log(identificador);
+
+fetch(`https://dummyjson.com/recipes/${identificador}`)
+    .then(function(response){
+     return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let nombre = document.querySelector("h1");
+        nombre.innerText = data.name;
+        let instrucciones = document.querySelector(".inspreparacion");
+        instrucciones.innerText = data.status;
+        let coccion = document.querySelector(".coccion");
+        coccion.innerText = data.species
+        let imagen = document.querySelector("img");
+        imagen.src = data.image;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
