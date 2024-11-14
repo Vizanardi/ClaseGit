f21226a6e093b10da6838a94d773248b5a219a67
MartinaSerra <mserranesci@udesa.edu.ar>
2024-11-14 18:07:01 -0300

    Agrege algo al js de search-reuslts

--- js/search-results.js
+++ js/search-results.js
@@ -4,5 +4,25 @@ let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
 
 let buscador = queryStringObj.get("q");
-
-
+fetch('https://dummyjson.com/recipes/search?q=Margherita')
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    let infoReceta = document.querySelector(".info-recetas","article")
+    let receta = ""
+    for (i=0; i<10; i++ ){
+        receta += `<article>
+                <img src="${data.results[i].image}" alt="" class="" class="">
+                <h2>${data.results[i].name}</h2>
+                <p>Nivel de dificultad: ${data.results[i].difficulty}</p>
+                <p> <a href= "./receta.html"></a> Ver mas </p>
+            </article>`;
+    console.log(receta) 
+    }
+    infoReceta.innerHTML = receta;
+})
+.catch(function(error){
+    console.log(error);
+})
