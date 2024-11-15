d597d444eb46b52957e3a4fc284f6bc8635a4e48
Vizanardi <mzanardiocampo@udesa.edu.ar>
2024-11-15 12:58:19 -0300

    Arreglo cargar mas del index

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
-fetch('https://dummyjson.com/recipes?limit=10&skip=10')
+let cantidadDeRecetas 
+let formulario = document.querySelector("form")
+let completaFormulario = document.querySelector("#buscador")
+
+formulario.addEventListener("submit", function(evento){
+    evento.preventDefault()
+    if (completaFormulario.value == ""){
+        console.log("buscador vacio")
+        completaFormulario.innerHTML = "<p>Complete el campo porfavor</p>"
+    }else if (formulario.value.length < 3)
+        completaFormulario.innerHTML = "<p>Que contenga al menos 3 caracteres</p>"
+})
+
+
+fetch('https://dummyjson.com/recipes?limit=10')
 .then(function(response) {
   return response.json();
 })
@@ -7,7 +21,8 @@ fetch('https://dummyjson.com/recipes?limit=10&skip=10')
     console.log(data);
     let infoReceta = document.querySelector(".info-recetas")
     let receta = "";
-    for (i=0; i<10; i++){
+    cantidadDeRecetas = data.recipes.length
+    for (i=0; i<data.recipes.length; i++){
         receta += `<article>
                 <img src=${data.recipes[i].image} alt="">
                 <h2>${data.recipes[i].name}</h2>
@@ -16,14 +31,38 @@ fetch('https://dummyjson.com/recipes?limit=10&skip=10')
             </article>`;
     console.log(receta)
     infoReceta.innerHTML = receta; 
-    }
-    let cargarMas = document.querySelector("button")
+    }  
+})
+
+.catch(function(error){
+    console.log(error);
+})
+    
+let cargarMas = document.querySelector(".cargarMas")
     cargarMas.addEventListener("click", function(){
-    receta += `<article>
-                <img src="${data.recipes[i].image}" alt="" class="" class="">
-                <h2>${data.recipes[i].name}</h2>
-                <p>Nivel de dificultad: ${data.recipes[i].difficulty}</p>
-                <p> <a href= "./receta.html"></a> Ver mas </p>
-            </article>`;
-    console.log(receta)
-})});
+        fetch(`https://dummyjson.com/recipes?limit=10&skip=${cantidadDeRecetas}`)
+        .then(function(response) {
+          return response.json();
+        })
+        
+        .then(function(data){
+            console.log(data);
+            let infoReceta = document.querySelector(".info-recetas")
+            let receta = "";
+            for (i=0; i<data.recipes.length; i++){
+                receta += `<article>
+                        <img src=${data.recipes[i].image} alt="">
+                        <h2>${data.recipes[i].name}</h2>
+                        <p>Nivel de dificultad: ${data.recipes[i].difficulty}</p>
+                        <p> <a href= "./receta.html?id=${data.recipes[i].id}"> Ver mas </a></p>
+                    </article>`;
+            console.log(receta)
+        }  
+        infoReceta.innerHTML += receta;
+        cantidadDeRecetas += 10 
+        })
+        
+        .catch(function(error){
+            console.log(error);
+        })
+    })
