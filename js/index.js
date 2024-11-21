37a84a1cec3f468f34773ffd1aa51e1d6548695a
Vizanardi <mzanardiocampo@udesa.edu.ar>
2024-11-21 19:27:40 -0300

    Trabajamos en el css del index todas juntas

--- js/index.js
+++ js/index.js
@@ -35,7 +35,7 @@ fetch('https://dummyjson.com/recipes?limit=10')
                 <img src=${data.recipes[i].image} alt="">
                 <h2 class="tituloReceta">${data.recipes[i].name}</h2>
                 <p>Nivel de dificultad: ${data.recipes[i].difficulty}</p>
-                <p> <a href= "./receta.html?id=${data.recipes[i].id}"> Ver mas </a></p>
+                <p> <a class="verMas" href= "./receta.html?id=${data.recipes[i].id}"> Ver mas </a></p>
             </article>`;
     console.log(receta)
     infoReceta.innerHTML = receta; 
